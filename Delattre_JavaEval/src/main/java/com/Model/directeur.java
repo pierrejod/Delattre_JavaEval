82c4d642fa63124ed87cae7f10f703ab147ad333
delattre <pierrejo74@gmail.com>
2021-06-11 16:19:46 +0200

    Commit class directeur

--- Delattre_JavaEval/src/main/java/com/Model/directeur.java
+++ Delattre_JavaEval/src/main/java/com/Model/directeur.java
@@ -1,5 +1,84 @@
 package com.Model;
 
-public class directeur {
+import java.io.Serializable;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="directeur")
+public class directeur implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id", nullable = false, unique =true)
+	private int id;
+	
+	@Column(name="nom", nullable = true, unique=false, length=50)
+	private String nom;
+	
+	@Column(name="prenom", nullable = true, unique=false, length=50)
+	private String prenom;
+	
+	@OneToMany(cascade=CascadeType.ALL)
+	@JoinColumn(name="client_id")
+	private Set<salarie> salaries;
+	
+	@OneToOne(mappedBy="directeur")
+	private hotel hotel;
+
+	public directeur() {
+		super();
+	}
+
+	public directeur(String nom, String prenom, Set<salarie> salaries) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.salaries = salaries;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Set<salarie> getSalaries() {
+		return salaries;
+	}
+
+	public void setSalaries(Set<salarie> salaries) {
+		this.salaries = salaries;
+	}
+		
 }
